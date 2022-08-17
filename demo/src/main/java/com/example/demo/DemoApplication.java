f48e514711bcf547699286b9339cfb7a7926386f
SURAJ KALLOLI <surajskalloli@gmail.com>
2022-08-17 12:56:44 +0530

    spring application

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@RestController
+
+public class DemoApplication {
+	@GetMapping("/message")
+	public String message() {
+		return "your app deployed succesfuly" ;
+	}
+
+
+	public static void main(String[] args)
+	{
+		
+		  SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
